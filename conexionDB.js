27874441a820cb0dd9a4f65a2b692c5c2de4e218
Johanna Riquelme <riquelme.johanna07@gmail.com>
2023-11-09 10:34:16 -0300

    agrego tabla en conexionDB.js

--- conexionDB.js
+++ conexionDB.js
@@ -1,18 +1,28 @@
-import{createPool}from "mysql2/promise";
+import { createPool } from "mysql2/promise";
 import configuracion from "./configuración.js"
+
+const CREAR_TABLA = `
+CREATE TABLE nacimientos(
+    id_padron INT NOT NULL AUTO_INCREMENT,
+    sexo VARCHAR(30) NOT NULL,
+    nombre VARCHAR(30) NOT NULL,
+    apellido VARCHAR(30) NOT NULL,
+    peso FLOAT,
+    PRIMARY KEY (id_padron)
+    )`;
 
 console.log(configuracion.PORT)
 
-const pool=createPool(
+const pool = createPool(
     {
-    host:configuracion.HOST,
-    port:configuracion.PORT,
-    user:configuracion.USER,
-    password:configuracion.PASSWORD,
-    database:configuracion.NAME
+        host: configuracion.HOST,
+        port: configuracion.PORT,
+        user: configuracion.USER,
+        password: configuracion.PASSWORD,
+        database: configuracion.NAME
 
     }
 );
-
+await pool.query(CREAR_TABLA);
 console.log("CONEXION A BASE DE DATOS INICIADA")
 export default pool;

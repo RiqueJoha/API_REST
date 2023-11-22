b6160d00bca2efb079cb2cf5159c040060ccea72
Johanna Riquelme <riquelme.johanna07@gmail.com>
2023-11-21 21:57:50 -0300

    probando actualización

--- src/controladores/nacimientos.controladores.js
+++ src/controladores/nacimientos.controladores.js
@@ -7,9 +7,9 @@ import pool from "../conexionDB.js"
  */
 
 async function obtenerNacimientos(req, res) {
-    const ID=req.params.id_padron;
+    const ID = req.params.id_padron;
     try {
-        const [resultado] = await pool.query("SELECT * FROM nacimientos ");
+        const [resultado] = await pool.query("SELECT * FROM nacimientos ", ID);
         /*   console.log(info); */
         if (!resultado.length) {
             res.status(404).json(
@@ -44,7 +44,7 @@ async function obtenerNacimiento(req, res) {
     const ID = req.params.id_padron;
 
     try {
-        const [resultado] = await pool.query("SELECT * FROM nacimientos WHERE id_padron= ? ", req.params.id_padron);
+        const [resultado] = await pool.query("SELECT * FROM nacimientos WHERE id_padron= ? ", ID);
         /* res.send(`<h1>Ruta usada 2 | id: ${req.params.id}</h1>`) */
         if (!resultado.length) {
             res.status(404).json(
@@ -99,21 +99,21 @@ async function crearNacimientos(req, res) {
 };
 
 async function actualizarNacimientos(req, res) {
- /*    console.log(req.query);
-    const ID = req.params.id_padron; */
+    console.log(req.query);
+    const ID = req.params.id_padron;
     console.log(ID);
     try {
         const { sexo, nombre, apellido, peso } = req.body;
         const [info] = await pool.query(`UPDATE nacimientos SET sexo=?, nombre=?,apellido=?, peso=?  WHERE id_padron=?;`, [sexo, nombre, apellido, peso, ID]);
-       /*  console.log(info)
-        res.send(`<h1>Ruta para actualizar un elemento</h1>`) */
+        console.log(info)
+        /*  res.send(`<h1>Ruta para actualizar un elemento</h1>`) */
         if (info.affectedRows !== 1 || info.warningStatus !== 0) {
             res.status(404).json({
-                info:"Error al actualizar el dato con id:"+ ID
+                info: "Error al actualizar el dato con id:" + ID
             })
         } else {
             res.json({
-                info:"Producto actualizado"
+                info: "Producto actualizado"
             });
         }
     } catch (error) {
@@ -128,22 +128,22 @@ async function actualizarNacimientos(req, res) {
 }
 
 async function eliminarNacimientos(req, res) {
-/*     console.log(req.query);
-    res.send(`<h1>Ruta para eliminar un elemento</h1>`) */
-    const ID=req.params.id_padron;
+    /*     console.log(req.query);
+        res.send(`<h1>Ruta para eliminar un elemento</h1>`) */
+    const ID = req.params.id_padron;
     try {
-        const [resultado] = await pool.query("DELETE FROM nacimientos where id_padron= ?;",[ID]);
+        const [resultado] = await pool.query("DELETE FROM nacimientos where id_padron= ?;", [ID]);
         /*   console.log(info); */
         console.log(resultado);
-        if (resultado.affectedRows!==1) {
+        if (resultado.affectedRows !== 1) {
             res.status(404).json(
                 {
-                    mensaje: "No se encontraron id "+ ID
+                    mensaje: "No se encontraron id " + ID
                 }
             )
         } else {
             res.json(
-            {info:"Nacimiento con id: "+ ID + " eliminado"}
+                { info: "Nacimiento con id: " + ID + " eliminado" }
             )
         }
     } catch (error) {

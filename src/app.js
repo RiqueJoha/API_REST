7bde8431f62eeb2a096bbf23f69ca4d55092405a
Johanna Riquelme <riquelme.johanna07@gmail.com>
2023-11-22 21:11:12 -0300

    control de acceso implementado

--- src/app.js
+++ src/app.js
@@ -4,14 +4,27 @@ import nacimientosRuta from "./rutas/nacimientos.rutas.js";
 
 const app = express();
 
-app.use(express.json());
+const urlPermitidas=["http://127.0.0.1:5500/"];
+
+app.use(express.json({
+    origin: (origin, callback) => {
+        console.log(origin);
+        if (urlPermitidas.includes(origin)||!origin) {
+            callback(null,true);
+        } else{
+            const error=new Error("Error no permitido por CORS");
+            error.status=401;
+            callback("Error Cors",false);
+        }   
+    }
+}));
 app.use(cors())
 
-app.use("/nacimientos/",nacimientosRuta)
+app.use("/nacimientos/", nacimientosRuta)
 
-   /*  console.log(req.body)  */
-   /*  res.send("<h1>Ruta vacia 0</h1>") */
-   
+/*  console.log(req.body)  */
+/*  res.send("<h1>Ruta vacia 0</h1>") */
+
 app.use((req, res) => {
     res.send("<h1>Error</h1>")
 })
